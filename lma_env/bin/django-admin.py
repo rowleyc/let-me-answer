df6001ee8a9a9c57d63684d0222d076c1d79d05e
Cody Rowley <rowleyc@email.arizona.edu>
2016-03-10 15:06:33 -0500

    First commit

---
+++ lma_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/spongebob/Documents/Programming/Python/Projects/web_apps/let_me_answer/lma_env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
